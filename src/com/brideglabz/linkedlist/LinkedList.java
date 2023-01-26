6a171381c8aec369e6c4ed93ca98092029ae7fc2
usha-12 <usha12ahirwar@gmail.com>
2023-01-26 16:58:45 +0530

    Added: welcome to the linkedlist program

--- src/com/brideglabz/linkedlist/LinkedList.java
+++ src/com/brideglabz/linkedlist/LinkedList.java
@@ -1,2 +1,7 @@
-package com.brideglabz.linkedlist;public class LinkedList {
+package com.brideglabz.linkedlist;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("-------Welcome to the LinkedList program-------");
+    }
 }
