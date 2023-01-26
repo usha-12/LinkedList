8d28978d2b77692a88af2431ef95578fd0d3b2fa
usha-12 <usha12ahirwar@gmail.com>
2023-01-26 17:14:16 +0530

    Added:Create linked list by adding 30 and 56 to 70

--- src/com/brideglabz/linkedlist/LinkedListMain.java
+++ src/com/brideglabz/linkedlist/LinkedListMain.java
@@ -2,15 +2,26 @@ package com.brideglabz.linkedlist;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("---------Welcome to the LinkedList program---------");
-        LinkedList<Integer> list1 = new LinkedList();
-        System.out.println("--------------Create Linked List----------------");
-        list1.push(56);
-        list1.push(30);
-        list1.push(70);
-        list1.print();
+            System.out.println("---------Welcome to the LinkedList program---------");
+            LinkedList<Integer> list1 = new LinkedList();
+            System.out.println("--------------Create Linked List----------------");
+            list1.add(70);
+            list1.add(30);
+            list1.add(56);
+            list1.print();
+            System.out.println("--------Add data in Linked List---------");
+            LinkedList<Integer> list2 = new LinkedList<>();
+            list2.push(70);
+            list2.push(30);
+            list2.push(56);
+            list2.print();
+        }
     }
-}
-/*Let's create a
-simple Linked List
-of 56, 30 and 70*/
+
+/*Ability to create Linked
+List by adding 30 and 56
+to 70
+- Node with data 70 is First Created
+- Next 30 is added to 70
+- Finally 56 is added to 30
+- LinkedList Sequence: 56->30->70*/
